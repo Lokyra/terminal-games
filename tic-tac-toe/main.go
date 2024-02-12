9e2e69cfa931c0119c3cc0d60ab3394b1d1702ed
ayoub <ayoubousbia02@gmail.com>
2024-02-12 17:13:59 +0100

    add board game

--- tic-tac-toe/main.go
+++ tic-tac-toe/main.go
@@ -25,9 +25,40 @@ func printMatix(matrix [3][3]int) {
 	}
 }
 
-func main() {
-	fmt.Println("Hello World!")
+func askNumber() {
+	var run bool = true
+	var n string = " "
+	for run {
+		fmt.Print("Enter a value : ")
+		fmt.Scan(&n)
+		if n == "O" {
+			run = false
+		}
+	}
+}
 
+func displayBoard(board [3][3]string) {
+	fmt.Printf(" %s  | %s  | %s \n", board[0][0], board[0][1], board[0][2])
+	fmt.Println("--------------")
+	fmt.Printf(" %s  | %s  | %s \n", board[1][0], board[1][1], board[1][2])
+	fmt.Println("--------------")
+	fmt.Printf(" %s  | %s  | %s ", board[2][0], board[2][1], board[2][2])
+}
+
+func main() {
+	fmt.Println("Welcome to The Tic-Tac-Toe game !")
+	fmt.Println()
+	board := [3][3]string{
+		{" ", " ", " "},
+
+		{" ", " ", " "},
+
+		{" ", " ", " "},
+	}
+
+	displayBoard(board)
+
+	// askNumber()
 	//	var numbers [10]int
 	// var matrix [3][3]int
 }
