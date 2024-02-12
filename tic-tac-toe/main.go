c758c6e95d00a2aa61574ab0e59e0411015da4d6
ayoub <ayoubousbia02@gmail.com>
2024-02-12 16:14:17 +0100

    add start playing with array and matices

--- tic-tac-toe/main.go
+++ tic-tac-toe/main.go
@@ -8,6 +8,26 @@ func welcome() {
 	fmt.Println("Welcome To Tic-Tac-Toe games")
 }
 
+func printArray(numbers [10]int) {
+	for i := 0; i < 10; i++ {
+		numbers[i] = i + 1
+		fmt.Println(numbers[i])
+	}
+}
+
+func printMatix(matrix [3][3]int) {
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			matrix[i][j] = j + 1
+			fmt.Print(matrix[i][j], " ")
+		}
+		fmt.Println()
+	}
+}
+
 func main() {
 	fmt.Println("Hello World!")
+
+	//	var numbers [10]int
+	// var matrix [3][3]int
 }
